b6122b567a334aa71f4a8c9f4b1d3db10277aef6
Gaston <74564178+gastoncabrera@users.noreply.github.com>
2023-01-03 16:25:00 -0300

    Feat: hcange maintsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { render } from "react-dom";
 
 import Lector from './Lector'
 import Route from "./Route"
 import './index.css'
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <Lector /> */}
-    <Route/>
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     {/* <Lector /> */}
+//     <Route/>
+//   </React.StrictMode>,
+// )
+
+
+const root = document.getElementById("root");
+render(<Route />, root);
